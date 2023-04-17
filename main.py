2aab9164f235582552a61f45b5e3c020631308e9
Xavitheforce <javier.miguelez.yague@gmail.com>
2023-04-17 17:21:07 +0200

    meter el enunciado

--- main.py
+++ main.py
@@ -29,3 +29,32 @@ if __name__ == '__main__':
     time.sleep(1)
     print ('Todos los clientes de hoy han sido atendidos')
     os._exit(0)
+
+
+
+
+
+
+    '''El objetivo de la práctica es simular una gasolinera bajo las siguientes premisas.
+ A la gasolinera llegan coches a un intervalo aleatorio de hasta T minutos.
+ La gasolinera consta de N surtidores de combustible.
+ Cuando un coche se pone en la surtidor de combustible, el conductor se baja,
+elije el combustible de su elección y llena el depósito. Este trabajo le lleva un
+tiempo de entre 5 y 10 minutos.
+ Tras llenar el depósito se acerca a la oficina de pago y se pone a la cola de la
+caja. Suponga que la caja es única.
+ En pagar tarda 3 minutos.
+ Tras terminar el pago retira el coche, dejando el surtidor libre para el siguiente
+coche.
+Realización
+Se pide:
+ Modelar el problema con los objetos apropiado indicando los estados en que
+puede estar cada elemento.
+ Se modelarán los coches como Threads que genera el programa principal. A
+efectos del ejercicio se generan 50 coches.
+ Realizar el problema para un tiempo T de 15 minutos y N de un surtidor de
+combustible.
+ Calcular el tiempo medio que tarda un coche desde que llega a la gasolinera
+hasta que sale de ella.
+ A efectos del problema los minutos se tratarán en el programa con centésimas
+de segundo.'''
